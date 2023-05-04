66a9473e0a160b04f0dd4b748fb1d0e0d8683c96
Pierre-Louis Furon <pierre-louis.furon@fuga.com>
2023-05-04 18:26:21 +0200

    it is possible to edit prep and cooking time from the forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,47 @@
 class Recipe < ApplicationRecord
-    has_many :recipe_tags
-    has_many :tags, :through => :recipe_tags
+    has_and_belongs_to_many :tags, join_table: :recipe_tags
+    # has_many :recipe_tags
+    # has_many :tags, :through => :recipe_tags
 
     has_many :recipe_ingredients
     has_many :ingredients, :through => :recipe_ingredients
+
+    attribute :prep_time, :integer, default: 0
+    attribute :cooking_time, :integer, default: 0
+
+    def prep_time_days
+        if prep_time != 0
+            prep_time / (24 * 60)
+        end
+    end 
+
+    def prep_time_hours
+        if prep_time != 0
+            (prep_time / 60) % 24
+        end
+    end
+
+    def prep_time_minutes
+        if prep_time != 0
+            prep_time % 60
+        end
+    end
+
+    def cooking_time_days
+        if cooking_time != 0
+            cooking_time / (24 * 60)
+        end
+    end 
+
+    def cooking_time_hours
+        if cooking_time != 0
+            (cooking_time / 60) % 24
+        end
+    end
+
+    def cooking_time_minutes
+        if cooking_time != 0
+            cooking_time % 60
+        end
+    end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
-    has_many :recipe_tags
-    has_many :recipes, :through => :recipe_tags
+    has_and_belongs_to_many :recipes, join_table: :recipe_tags
+    # has_many :recipe_tags
+    # has_many :recipes, :through => :recipe_tags
 end
